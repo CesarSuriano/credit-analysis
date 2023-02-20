8cba4b2bb09833cb542d6a895030b163b61e0f9f
cesarb <cesarb@grupopanvel.com.br>
2023-02-20 06:00:22 -0300

    feat: criando a tela Home

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
+import { CreditAnalysisService } from 'src/app/shared/services/credit-analysis.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  searchText!: string;
+  isLoading: boolean = false;
+  hasError: boolean = false;
+
+  constructor(private primengConfig: PrimeNGConfig, private creditAnalysisService: CreditAnalysisService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+  }
+
+  searchId(): void {
+    this.isLoading = true;
+    this.creditAnalysisService.getCreditAnalysis().subscribe(data => {
+      setTimeout(() => {
+        this.isLoading = false;
+        if (data.uuid === this.searchText) {
+          this.router.navigate(['analysis-viewer']);
+          return;
+        }
+        this.hasError = true;
+      }, 1500);
+    });
+  }
+}
