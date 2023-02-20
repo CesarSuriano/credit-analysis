e321c527dd4160152043af5baa1900e89eaae59b
cesarb <cesarb@grupopanvel.com.br>
2023-02-20 05:13:03 -0300

    feat: alterações no componente app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'analysis-viewer',
+    loadChildren: () => import('./pages/analysis-viewer/analysis-viewer.module').then(m => m.AnalysisViewerModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
